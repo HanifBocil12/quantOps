893099b903b0bd9176bc86cadc10b0343943a0b3
HanifBocil12 <hanifbocil45@gmail.com>
2026-07-28 14:23:20 +0700

    babi

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,9 @@ Route::get('/model', [pageContoller::class, 'model'])->name('model');
 Route::get('/execution', [pageContoller::class, 'execution'])->name('execution');
 Route::get('/laporan', [pageContoller::class, 'laporan'])->name('laporan');
 Route::get('/trade/{pair}', [pageContoller::class, 'trade'])->name('trade');
-Route::get('/system/fetch-news', function () {
+Route::match(['get', 'post'], '/system/fetch-news', function () {
 
     Artisan::call('telegram:fetch-news');
 
-    return response()->json([
-        'status' => 'ok'
-    ]);
+    return 'ok';
 });
