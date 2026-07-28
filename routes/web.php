eec2633979f46cae743a0cdf2205391e27982f29
HanifBocil12 <hanifbocil45@gmail.com>
2026-07-28 14:17:39 +0700

    get

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/model', [pageContoller::class, 'model'])->name('model');
 Route::get('/execution', [pageContoller::class, 'execution'])->name('execution');
 Route::get('/laporan', [pageContoller::class, 'laporan'])->name('laporan');
 Route::get('/trade/{pair}', [pageContoller::class, 'trade'])->name('trade');
-Route::post('/system/fetch-news', function () {
+Route::get('/system/fetch-news', function () {
+
     Artisan::call('telegram:fetch-news');
 
-    return 'ok';
+    return response()->json([
+        'status' => 'ok'
+    ]);
 });
